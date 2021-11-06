2a90d37cc4a9affb88f99ffb0cb89a58f814323f
Jonathan Shaw <jonathan.shaw@posteo.net>
2021-11-05 21:01:07 -0700

    commit prior to building models

--- helpdesk/models.py
+++ helpdesk/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Queue(models.Model):
+    pass
+
+class Ticket(models.Model):
+    pass
+
+class Comment(models.Model):
+    pass
